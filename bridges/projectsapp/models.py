a0d06af4f81ed73c3f0344b797d92cb65fd413d5
ghost211221 <solovyev.21@yandex.ru>
2019-09-28 14:54:13 +0300

    started models

--- bridges/projectsapp/models.py
+++ bridges/projectsapp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
+
+
 # Create your models here.
+class Project(models.Model):
+    name = models.CharField(verbose_name='название', max_length=256, unique=True)
+    description = models.TextField(verbose_name='описание', blank=True)
+    image = models.ImageField(upload_to='projects_images', blank=True)
+    finishDate = models.DateTimeField(verbose_name=_('Дата сдачи'), blank=False)
+    techDes = models.ForeignKey(techDes, on_delete=models.CASCADE)
+    address = models.CharField(max_length=512, blank=True, null=True)
+    executor = models.CharField(max_length=512, blank=True, null=True)
+    orderer = models.CharField(max_length=512, blank=True, null=True)
+    designer = models.CharField(max_length=512, blank=True, null=True)
+
+
