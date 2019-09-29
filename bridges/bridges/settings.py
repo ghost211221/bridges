629984d1735b2effc77a92e2a9d3aeb087a5c60a
ghost211221 <solovyev.21@yandex.ru>
2019-09-29 21:17:19 +0300

    temp

--- bridges/bridges/settings.py
+++ bridges/bridges/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'mainapp',
     'productsapp',
+    'projectsapp',
     'contactapp',
 ]
 
--- bridges/bridges/urls.py
+++ bridges/bridges/urls.py
@@ -22,7 +22,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', include('mainapp.urls', namespace='main')),
     path('contact/', include('contactapp.urls', namespace='contact')),
-    path('products', include('productsapp.urls', namespace='products')),
+    path('products/', include('productsapp.urls', namespace='products')),
+    path('projects/', include('projectsapp.urls', namespace='projects')),
     path('admin/', admin.site.urls),
 ]
 
