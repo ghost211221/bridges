a0529c33aa922737b94aa5043e5e1c30837957e4
evgeny <djoongaar@gmail.com>
2019-09-29 13:01:49 +0300

    contactapp is done

--- bridges/productsapp/views.py
+++ bridges/productsapp/views.py
@@ -2,8 +2,4 @@ from django.shortcuts import render
 
 
 def products(request):
-    title = "Технические решения"
-    technical_solutions = TechnicalSolutions.objects.all()
-
-    content = {'title': title, 'products': technical_solutions}
-    return render(request, 'productsapp/products.html', content)
+    return render(request, 'productsapp/products.html')
