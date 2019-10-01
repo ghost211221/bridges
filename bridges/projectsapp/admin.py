394960fa1f5b5da958ec67df63a0836a9f32e3d0
Vasily <dorican80@gmail.com>
2019-10-01 23:23:15 +0300

    projects gallery

--- bridges/projectsapp/admin.py
+++ bridges/projectsapp/admin.py
@@ -2,8 +2,21 @@ from django.contrib import admin
 from .models import *
 
 
+class ProjectImageInline(admin.TabularInline):
+    model = ProjectImage
+    extra = 0
+
+
 @admin.register(Project)
 class ProjectAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
     list_display = ('name', 'city',)
     list_display_links = ('name', 'city',)
     search_fields = ('name',)
+    inlines = [ProjectImageInline]
+
+
+@admin.register(ProjectImage)
+class ProjectImageAdmin(admin.ModelAdmin):
+    list_display = ('id', 'image', 'is_active', 'created', 'updated',)
+    list_display_links = ('id', 'image',)
