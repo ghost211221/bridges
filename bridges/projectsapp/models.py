fbf3941b8f1da4dc81f7dce6994ce9ec49fde7c3
evgeny <djoongaar@gmail.com>
2019-10-01 07:56:23 +0300

    projectsapp models are done

--- bridges/projectsapp/models.py
+++ bridges/projectsapp/models.py
@@ -2,17 +2,20 @@ from django.db import models
 from productsapp.models import TechnicalSolutions
 
 
-
 # Create your models here.
 class Project(models.Model):
     name = models.CharField(verbose_name='название', max_length=256, unique=True)
     description = models.TextField(verbose_name='описание', blank=True)
     image = models.ImageField(upload_to='projects_images', blank=True)
-    finishDate = models.DateTimeField()
-    techDes = models.ForeignKey(TechnicalSolutions, on_delete=models.CASCADE)
-    address = models.CharField(max_length=512, blank=True, null=True)
-    executor = models.CharField(max_length=512, blank=True, null=True)
-    orderer = models.CharField(max_length=512, blank=True, null=True)
-    designer = models.CharField(max_length=512, blank=True, null=True)
+    finishDate = models.DateTimeField(auto_now_add=True, auto_now=False)
+    city = models.CharField(verbose_name='город', max_length=512, blank=True, null=True)
+    address = models.CharField(verbose_name='адрес', max_length=512, blank=True, null=True)
+    latitude = models.FloatField(verbose_name='широта', null=True)
+    longitude = models.FloatField(verbose_name='долгота', null=True)
+    contractor = models.CharField(verbose_name='подрядчик', max_length=512, blank=True, null=True)
+    customer = models.CharField(verbose_name='заказчик', max_length=512, blank=True, null=True)
+    designer = models.CharField(verbose_name='проектировщик', max_length=512, blank=True, null=True)
+    technical_solutions = models.ManyToManyField(TechnicalSolutions)
 
-
+    def __str__(self):
+        return f"{self.name} ({self.city})"
