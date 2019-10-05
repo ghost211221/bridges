dd8fd909e7974dcb1e66d73a616539029380206c
evgeny <djoongaar@gmail.com>
2019-10-05 13:48:32 +0300

    research documents filter works

--- bridges/researchapp/views.py
+++ bridges/researchapp/views.py
@@ -4,7 +4,9 @@ from researchapp.models import Document
 
 
 def research(request):
-    documents = Document.objects.all()
+    documents = Document.objects.filter(author__id=1)
+    # documents = Document.objects.all()
+
     content = {
         'documents': documents,
     }
