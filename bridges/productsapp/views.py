c21cb848d178e13569a14401a7d4fcd50fad450a
evgeny <djoongaar@gmail.com>
2019-10-05 14:33:29 +0300

    research documents added on product page

--- bridges/productsapp/views.py
+++ bridges/productsapp/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from productsapp.models import TechnicalSolutions, Material
 from projectsapp.models import ProjectHasTechnicalSolutions
 from projectsapp.models import Project
+from researchapp.models import Document
 
 
 def products(request):
@@ -21,11 +22,16 @@ def product(request, slug):
     item = get_object_or_404(TechnicalSolutions, slug=slug)
     material_list = item.material_content.all()
     project_list = ProjectHasTechnicalSolutions.objects.filter(techsol__pk=item.pk)
+    researches = Document.objects.filter(techsol__pk=item.pk).exclude(type__id=1)
+    documents = Document.objects.filter(techsol__pk=item.pk).exclude(type__in=[2, 3])
+
 
     content = {
         'page_title': title,
         'product': item,
         'material_list': material_list,
-        'projects': project_list
+        'projects': project_list,
+        'researches': researches,
+        'documents': documents
     }
     return render(request, 'productsapp/product.html', content)
--- bridges/researchapp/views.py
+++ bridges/researchapp/views.py
@@ -4,10 +4,15 @@ from researchapp.models import Document
 
 
 def research(request):
-    documents = Document.objects.filter(author__id=1)
+    researches = Document.objects.all().exclude(type__id=1)
+    documents = Document.objects.all().exclude(type__in=[2, 3])
+
+    # documents = Document.objects.filter(author__in=[1,]).exclude(type__id=2)
     # documents = Document.objects.all()
 
     content = {
+        'researches': researches,
         'documents': documents,
     }
+
     return render(request, 'researchapp/research.html', content)
