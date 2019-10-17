ec10fb62b3d1f18bbfd57ea6da008f525c0b8b96
Vasily <dorican80@gmail.com>
2019-10-17 13:34:18 +0300

    authapp css

--- bridges/authapp/urls.py
+++ bridges/authapp/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
-
-from .views import RegisterUserView, UserLoginView, UserProfileView, UserLogoutView
+# from .views import RegisterUserView, UserLoginView, UserProfileView, UserLogoutView
+from .views import RegisterUserView, UserLoginView, UserLogoutView
+from authapp import views as authapp
 
 app_name = 'authapp'
 
 urlpatterns = [
     path('register/', RegisterUserView.as_view(), name='register'),
     path('login/', UserLoginView.as_view(), name='login'),
-    path('profile/<int:pk>/', UserProfileView.as_view(), name='profile'),
+    # path('profile/<int:pk>/', UserProfileView.as_view(), name='profile'),
+    path('profile/<int:pk>/', authapp.profile, name='profile'),
     path('logout/', UserLogoutView.as_view(), name='logout'),
 ]
--- bridges/authapp/views.py
+++ bridges/authapp/views.py
@@ -1,13 +1,11 @@
+from django.shortcuts import render, get_object_or_404
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView
 from django.contrib.auth.views import LoginView, LogoutView
 from django.views.generic.detail import DetailView
 from .forms import RegisterUserForm, LoginUserForm
-
 from .models import Users
 
-
-# Create your views here.
 
 class RegisterUserView(CreateView):
     model = Users
@@ -29,17 +27,26 @@ class UserLoginView(LoginView):
     template_name = 'authapp/login.html'
 
 
-class UserProfileView(DetailView):
-    model = Users
-    extra_context = {
+# class UserProfileView(DetailView):
+#     model = Users
+#     extra_context = {
+#         'page_title': 'Профиль пользователя',
+#         'bred_title': 'Профиль пользователя'
+#     }
+#     template_name = 'authapp/profile.html'
+#
+#     def get_context_data(self, *args, **kwargs):
+#         context = super().get_context_data(*args, **kwargs)
+#         return context
+
+def profile(request, pk):
+    user = get_object_or_404(Users, pk=pk)
+    context = {
+        'user': user,
         'page_title': 'Профиль пользователя',
         'bred_title': 'Профиль пользователя'
     }
-    template_name = 'authapp/profile.html'
-
-    def get_context_data(self, *args, **kwargs):
-        context = super().get_context_data(*args, **kwargs)
-        return context
+    return render(request, 'authapp/profile.html', context)
 
 
 class UserLogoutView(LogoutView):
