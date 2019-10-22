a1ea3d1c49dc2867d22b5542da1563eec644af7f
evgeny <djoongaar@gmail.com>
2019-10-22 11:33:18 +0300

    CreateMixin

--- bridges/projectsapp/utils.py
+++ bridges/projectsapp/utils.py
@@ -1,5 +1,11 @@
+from django.forms import modelformset_factory
+from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render, redirect
 from django.urls import reverse
+
+from authapp.models import Users
+from projectsapp.forms import ProjectManagerCreateForm
+from projectsapp.models import Project, ProjectManagers
 
 
 class ObjectCreateMixin:
@@ -16,3 +22,34 @@ class ObjectCreateMixin:
             bound_form.save()
             return redirect('projectsapp:projects')
         return render(request, self.template, context={'form': bound_form})
+
+
+class CreateMixin:
+    form_model = None
+    form = None
+    template = None
+
+    def get(self, request, project_pk):
+        project = Project.objects.get(pk=project_pk)
+        form = self.form(initial={"project": project})
+        context = {
+            'project_manager_form': form
+        }
+        return render(request, template_name=self.template, context=context)
+
+    def post(self, request, project_pk):
+        project = Project.objects.get(pk=project_pk)
+        form = self.form(request.POST)
+        if form.is_valid():
+            hacked = {
+                "project": Project.objects.get(pk=form.data["project"]),
+                "manager": Users.objects.get(pk=form.data["manager"])
+            }
+            data = {**form.data, **hacked}
+            data = {k: v[0] if isinstance(v, list) else v for k, v in data.items() if
+                    k in {f.name for f in self.form_model._meta.fields}}
+            data['is_active'] = True if data['is_active'] == 'on' else False
+            obj = self.form_model(**data)
+            obj.save()
+            return HttpResponseRedirect(project.get_absolute_url())
+        return HttpResponse(status=400)
--- bridges/projectsapp/views.py
+++ bridges/projectsapp/views.py
@@ -3,10 +3,11 @@ from django.forms import inlineformset_factory, modelformset_factory
 from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render, get_object_or_404
 
+from projectsapp.utils import CreateMixin
 from .forms import *
 from projectsapp.models import ProjectManagers
 from projectsapp.models import ProjectImage
-from django.views.generic import View
+from django.views.generic import View, CreateView
 from django.views.generic import ListView, DetailView
 from projectsapp.forms import ProjectSolutionsForm, ProjectManagerForm, ProjectCompanyForm
 from projectsapp.models import Project, ProjectHasTechnicalSolutions, ProjectCompany
@@ -123,59 +124,39 @@ def company_update(request, pk):
 
 #  ------------------------------------ PROJECT'S MANAGERS ----------------------------------------------
 
-
-class ProjectsManagerCreateView(View):
-    ProjectManagersFormSet = modelformset_factory(ProjectManagers, fields='__all__')
-
-    def get(self, request, project_pk):
-        form = ProjectManagerCreateForm(initial={"project": Project.objects.get(pk=project_pk)})
-        context = {
-            'project_manager_form': form
-        }
-        return render(request, template_name='projectsapp/project_manager_add.html', context=context)
-
-    def post(self, request, project_pk):
-        form = ProjectManagerCreateForm(request.POST)
-        if form.is_valid():
-            hacked = {
-                "project": Project.objects.get(pk=form.data["project"]),
-                "manager": Users.objects.get(pk=form.data["manager"])
-            }
-            data = {**form.data, **hacked}
-            data = {k: v[0] if isinstance(v, list) else v for k, v in data.items() if
-                    k in {f.name for f in ProjectManagers._meta.fields}}
-            data['is_active'] = True if data['is_active'] == 'on' else False
-            obj = ProjectManagers(**data)
-            obj.save()
-            return HttpResponseRedirect("/")
-        return HttpResponse(status=400)
-
-
 @user_passes_test(lambda u: u.is_superuser)
-def project_managers_update(request, pk):
-    project = get_object_or_404(Project, pk=pk)
-    project_form = ProjectForm(instance=project)
-    managers_formset = inlineformset_factory(Project, ProjectManagers, form=ProjectManagerForm, extra=1)
-    formset = managers_formset(instance=project)
-    if request.method == 'POST':
-        project_form = ProjectForm(request.POST, instance=project)
-        formset = managers_formset(request.POST)
-        if project_form.is_valid():
-            updated_project = project_form.save(commit=False)
-            formset = managers_formset(request.POST, instance=updated_project)
-            if formset.is_valid():
-                updated_project.save()
-                formset.save()
-                return HttpResponseRedirect(updated_project.get_absolute_url())
-    context = {
-        'project_form': project_form,
-        'formset': formset,
-        'page_title': 'Обновление списка участников',
-        'bred_title': 'Список участников',
-        'project': project,
-        'user': request.user
-    }
-    return render(request, 'projectsapp/company_update.html', context)
+class ProjectsManagerCreateView(CreateMixin, View):
+    form_model = ProjectManagers
+    form = ProjectManagerCreateForm
+    template = 'projectsapp/project_manager_add.html'
+    FormSet = modelformset_factory(form_model, fields='__all__')
+
+
+# @user_passes_test(lambda u: u.is_superuser)
+# def project_managers_update(request, pk):
+#     project = get_object_or_404(Project, pk=pk)
+#     project_form = ProjectForm(instance=project)
+#     managers_formset = inlineformset_factory(Project, ProjectManagers, form=ProjectManagerForm, extra=1)
+#     formset = managers_formset(instance=project)
+#     if request.method == 'POST':
+#         project_form = ProjectForm(request.POST, instance=project)
+#         formset = managers_formset(request.POST)
+#         if project_form.is_valid():
+#             updated_project = project_form.save(commit=False)
+#             formset = managers_formset(request.POST, instance=updated_project)
+#             if formset.is_valid():
+#                 updated_project.save()
+#                 formset.save()
+#                 return HttpResponseRedirect(updated_project.get_absolute_url())
+#     context = {
+#         'project_form': project_form,
+#         'formset': formset,
+#         'page_title': 'Обновление списка участников',
+#         'bred_title': 'Список участников',
+#         'project': project,
+#         'user': request.user
+#     }
+#     return render(request, 'projectsapp/company_update.html', context)
 
 
 #  ------------------------------------ PROJECT'S GALLERY ----------------------------------------------
