1d225167009a3e6eea77eefe755eee89c3cfc0df
Vasily <dorican80@gmail.com>
2019-11-04 13:13:38 +0300

    product in order model

--- bridges/ordersapp/views.py
+++ bridges/ordersapp/views.py
@@ -14,6 +14,7 @@ def order_create(request, pk):
 		if order_form.is_valid():
 			created_form = order_form.save(commit=False)
 			created_form.user = request.user
+			created_form.product = product
 			if count_orders == 0:
 				created_form.order_number = 1
 			else:
